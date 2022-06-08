1f84668691a33284f47bbe5c881ca9b01fee44a9
Constanza Oriana <cotyoriana95@gmail.com>
2022-06-07 22:29:01 -0300

    Agrego objeto constructor

--- app.js
+++ app.js
@@ -1,4 +1,14 @@
-alert("WELCOME GEEK! To start the game, let's know your top three favourite Stephen King's books");
+/*CONSTRUI ESTA PAGINA PARA HACER FUNCIONAR MEDIANTE JavaScript UNA DINAMICA DE TRIVIA
+EN DONDE EL USUARIO JUEGUE A ADIVINAR LAS RESPUESTAS CORRECTAS
+Y LUEGO EL SITIO CUENTE LA CANTIDAD DE ACIERTOS 
+ARROJANDO MENSAJES DIFERENTES DE ACUERDO AL RESULTADO OBTENIDO.*/
+
+
+/*Practicando algoritmo y simulador interactivo*/
+
+let userName = prompt("Enter your geek name");
+
+alert("WELCOME " + userName + "! " + "To start the game, let's know your top three favourite Stephen King's books");
 
 let userChoice = prompt("Your MOST favourite:");
 
@@ -10,16 +20,80 @@ while(userChoice != null){
 
 alert("This are your GOAL top three:" + "\n" + userChoice + "\n" + newUserChoice1 + "\n" + newUserChoice2);
 
+/*Practicando objeto constructor*/
+
+class Quiz{
+    constructor(question, option1, option2, option3, answer){
+        this.question = question;
+        this.option1 = option1;
+        this.option2 = option2;
+        this.option3 = option3;
+        this.answer = answer;
+    }
+    resultado(){
+
+    }
+}
+
+const one = new Quiz("What's the real name of Pennywise the clown?", "It", "Charlie", "Uncle Clown", "Bob");
+const two = new Quiz("Which story has an immortal mouse?", "The Shawshank Redemption", "Geralt's Game", "Misery", "The Green Mile");
+const three = new Quiz("What type of dog communicates with dead people in Under the Dome?", "Beagle", "Golden Retriever", "Rotwailler", "Corgi");
+const four = new Quiz("What does Annie Wilkes use in Phil's legs in Misery?", "Hammer", "Bat", "Piece of metal", "Axe");
+const five = new Quiz("Which room hosted a dead woman in The Shining?", "19", "256", "34", "237");
+const six = new Quiz("Name the actor/actress that played two mayor caracthers in King's movies", "Tom Hanks", "Jeffrey Demunn", "Melissa McBride", "Kathy Bates");
+const seven = new Quiz("Where did Carrie's story took place?", "Castle Rock", "Bangor", "Derry", "Chamberlain");
+const eight = new Quiz("What's the undead cat's name in Pet Sematary?", "Stacey", "Fluffy", "Doug", "Church");
+const nine = new Quiz("Color of the Mr Mercedes virtual meet-up umbrella logo", "Red", "Yellow", "Purple", "Blue");
+const ten = new Quiz("What's the purpose of Johnny Smith in The Dead Zone?", "His dog resurrection", "Save his wife from cancer", "Become a powerful politician", "Avoid nuclear war");
+
+console.log(seven.question)
 
 
-/*CONSTRUI ESTA PAGINA PARA HACER FUNCIONAR MEDIANTE JavaScript UNA DINAMICA DE TRIVIA
-EN DONDE EL USUARIO JUEGUE A ADIVINAR LAS RESPUESTAS CORRECTAS
-Y LUEGO EL SITIO CUENTE LA CANTIDAD DE ACIERTOS 
-ARROJANDO MENSAJES DIFERENTES DE ACUERDO AL RESULTADO OBTENIDO.*/
 
+//EJERCICIOS DE PRACTICA EN CLASES
 
+//  function calculadora (primerNum, segundoNum, operacion) {
+//     switch(operacion){
+//         case "+":
+//            return primerNum + segundoNum;
+//           break;
+//          case "-":
+//           return primerNum - segundoNum;
+//            break;
+//        case "*":
+//            return primerNum * segundoNum;
+//          break;
+//         case "/":
+//              primerNum / segundoNum;
+//             break;
+//        default:
+//            "No ha ingresado caracter valido";
+//          break;
+//      }
+//  }
 
-//EJERCICIOS DE PRACTICA
+//  let primerNum = Number(prompt("Ingrese primer numero"));
+// let segundoNum = Number(prompt("Ingrese segundo num"));
+//  let resultado = prompt("Ingrese simbolo de operacion");
+
+// alert("El resultado es" + "\n" + calculadora(primerNum, segundoNum, resultado))
+
+// let ingresarNumero = Number(prompt("Ingrese numero"));
+// for (let i = 1; i <= 10; i++) {
+//     let resultado = ingresarNumero * 1;
+//     alert(ingresarNumero + "x" + i + "=" + resultado);
+// }
+
+// let numero = 0;
+// do{
+//     numero = prompt("Ingrese num");
+//     console.log(numero);
+// } while (parseInt(numero));
+
+// let repetir = false; 
+// do{
+//     console.log("Solo una vez");
+// } while(repetir)
 
 /*let userName = prompt("Enter your geek name");
 
